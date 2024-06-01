66276a4bc2c248c9712bc47e300f985d259b83ea
barcek2281 <ajnat-2006@mail.ru>
2024-06-01 15:14:12 +0500

    final commit

--- urlshortner/shortner/models.py
+++ urlshortner/shortner/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.core.validators import URLValidator
+from rest_framework.exceptions import ValidationError
 
 
-# Create your models here.
 class Url(models.Model):
     link = models.CharField(max_length=10_000)
     uuid = models.CharField(max_length=10)
+
+    def is_valid(self) -> bool:
+        try:
+            URLValidator()(self.link)
+            return True
+        except :
+            return False
--- urlshortner/shortner/views.py
+++ urlshortner/shortner/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render, redirect
 import uuid
 from .models import Url
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+from .serializers import UrlSerializer
+
+
 def index(request):
     return render(request, 'index.html')
 
+
+@api_view(['POST'])
 def create(request):
-    if request.method == 'POST':
-        url = request.POST['link']
+    url = request.POST['link']
+    new_url = Url(link=url)
+    if new_url.is_valid():
         uid = str(uuid.uuid4())[:5]
-        new_url = Url(link=url, uuid=uid)
+        new_url = Url(link=url, uuid=uid, )
         new_url.save()
-        return HttpResponse(uid)
+        return JsonResponse({'valid': True, 'link': new_url.uuid})
+    return JsonResponse({'valid': False})
+
 
 def go(request, pk):
     url_details = Url.objects.get(uuid=pk)
